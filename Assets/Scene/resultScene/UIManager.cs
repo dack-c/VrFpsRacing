d4acc27e8261da057a73fe2d689063b411c545e7
queenanemone <shinhs9902@gmail.com>
2023-12-05 13:18:26 +0900

    Add button that can return to MainScene

---
+++ Assets/Scene/resultScene/UIManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public bool WinVar = false;
+    public bool LoseVar = false;
+    public bool RetireVar = false;
+
+    void Start()
+    {
+        if (WinVar || LoseVar)
+        {
+            
+        }
+        else if (RetireVar)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
