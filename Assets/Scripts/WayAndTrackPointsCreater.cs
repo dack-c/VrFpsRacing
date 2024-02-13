65d8b32330a440b7a194a38cf362cd2703df0e79
dack-c <qpxmaos3845@gmail.com>
2024-02-13 23:07:20 +0900

    바뀐 맵 적용 및 웨이포인트 생성로직 수정
    -4배 증가된 도로를 terrain test Scene_M2.unity에 적용
    -차의 앞을 기준으로 waypoint의 순서 재정렬하는 코드 추가

--- Assets/Scripts/WayAndTrackPointsCreater.cs
+++ Assets/Scripts/WayAndTrackPointsCreater.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using EasyRoads3Dv3;
 using System.Drawing;
+using System;
+using UnityEditor.Rendering.LookDev;
 
 public class WayAndTrackPointsCreater : MonoBehaviour
 {
@@ -30,10 +32,15 @@ public class WayAndTrackPointsCreater : MonoBehaviour
 
     public float trackPointCreationAngle = 70f; //이 각도 이상으로 꺾어지는 코너면 트랙포인트 생성
 
+    public GameObject carsParent;
 
     // Start is called before the first frame update
     void Awake()
     {
+
+        
+        //1. 웨이포인트 생성
+
         /*EasyRoad 프로버전에서만 사용가능한 스크립트
         roadNetwork = new ERRoadNetwork();
         ERRoad erRoad = roadNetwork.GetRoadByName(gameObject.name);
@@ -82,7 +89,7 @@ public class WayAndTrackPointsCreater : MonoBehaviour
                 GameObject waypointObj = Instantiate(waypointPrefab, centerPoint, Quaternion.identity);
                 waypoints.Add(waypointObj.transform);
                 rightEdgeForWaypoints.Add(colliderVertices[i]);
-                waypointObj.transform.parent = wayPointParent.transform;
+                //waypointObj.transform.parent = wayPointParent.transform;
                 if (waypointAttrList.Contains(th)&&manual)//수동 코너 지정
                 {
                     waypointObj.GetComponent<WaypointAttr>().ways.befCorner = true;
@@ -101,7 +108,46 @@ public class WayAndTrackPointsCreater : MonoBehaviour
             distance = 0;
         }
 
-        //자동 코너 지정
+
+        //2. 경주 시작점에 맞춰 waypoints 순서 재정렬
+
+        //2.1) finishLine기준으로 가장 멀리 있는 차 구하기
+        float longestDistance = 0f;
+        Transform frontmostCar = null;
+        for (int i = 0; i < carsParent.transform.childCount; i++)
+        {
+            Transform car = carsParent.transform.GetChild(i);
+            float curDistance = Vector3.Distance(finishLineObj.transform.position, car.position);
+
+            if (curDistance > longestDistance) // 위에서 잡은 기준으로 거리 재기
+            {
+                frontmostCar = car;
+                longestDistance = curDistance;
+            }
+        }
+
+        //2.2) 가장 멀리있는 차의 앞에 있는 웨이포인트 중에 가장 가까운 웨이포인트 찾기
+        int closestWaypointIndex = FindClosestAndForwardWaypointIndexFrom(frontmostCar);
+
+        // 2.3) 이 웨이포인트를 기준으로 waypoints와 rightEdgeForWaypoints를 재정렬
+        List<Transform> tempList = waypoints.GetRange(closestWaypointIndex, waypoints.Count - closestWaypointIndex);
+        waypoints.RemoveRange(closestWaypointIndex, waypoints.Count - closestWaypointIndex);
+        tempList.AddRange(waypoints);
+        waypoints = tempList;
+
+        List<Vector3> tempList2 = rightEdgeForWaypoints.GetRange(closestWaypointIndex, rightEdgeForWaypoints.Count - closestWaypointIndex);
+        rightEdgeForWaypoints.RemoveRange(closestWaypointIndex, rightEdgeForWaypoints.Count - closestWaypointIndex);
+        tempList2.AddRange(rightEdgeForWaypoints);
+        rightEdgeForWaypoints = tempList2;
+
+        // 2.4) 재정렬된 waypoint들을 부모에 귀속
+        foreach (Transform t in waypoints)
+        {
+            t.SetParent(wayPointParent.transform);
+        }
+
+
+        // 3. 자동 코너 지정
         if (!manual)
         {
             //웨이포인트 2개 사이의 코너 감지
@@ -160,6 +206,8 @@ public class WayAndTrackPointsCreater : MonoBehaviour
             }
         }
 
+
+        //4. 트랙포인트 생성 로직
         float reverseAngleSum = 0f;
         for(int i = 0; i < waypoints.Count - 1; i++)
         {
@@ -196,6 +244,55 @@ public class WayAndTrackPointsCreater : MonoBehaviour
             trackpointObj.transform.parent = trackPointParent.transform;
         }*/
     }
+
+    private int FindClosestAndForwardWaypointIndexFrom(Transform origin)
+    {
+        float closestDistance = 10000f;
+        int closestIndex = 0;
+        for(int i = 0; i < waypoints.Count; i++)
+        {
+            Vector3 directionToWaypoint = waypoints[i].position - origin.position;
+            float angle = Vector3.Angle(origin.forward, directionToWaypoint);
+            if(angle < 90)//이 웨이포인트가 origin의 앞에 있을 경우
+            {
+                float distance = Vector3.Distance(origin.position, waypoints[i].position);
+                if(distance < closestDistance)
+                {
+                    closestIndex = i;
+                    closestDistance = distance;
+                }
+            }
+        }
+        return closestIndex;
+    }
+
+    /*private int FindClosestAndForwardWaypointIndexFrom(Transform origin)// origin 앞에 있는 웨이포인트들 중에서 가장 가까운 웨이포인트 찾기 
+    {
+        int result = 0;
+        BinarySearchForClosestAndForwardWaypoint(origin, 0, waypoints.Count-1, ref result);
+        return result;
+    }
+
+    private void BinarySearchForClosestAndForwardWaypoint(Transform origin, int left, int right, ref int result)
+    {
+        if(left > right)
+        {
+            return;
+        }
+
+        int center = (left + right) / 2;
+        Vector3 directionToWaypoint = waypoints[center].position - origin.position;
+        float angle = Vector3.Angle(origin.forward, directionToWaypoint);
+        if (angle < 90)//이 웨이포인트가 origin의 앞에 있을 경우
+        {
+            result = center;
+            BinarySearchForClosestAndForwardWaypoint(origin, left, center-1, ref result); //앞에 있으면서 더 가까운 웨이포인트들이 있는지 탐색
+        }
+        else
+        {
+            BinarySearchForClosestAndForwardWaypoint(origin, center+1, right, ref result); //일단 뒤에 있으므로 앞에 있는 웨이포인트들을 향해 탐색
+        }
+    }*/
 
     private float CaculateWaypointAngle(int frontIndex, bool isReturnSigned)//이 index의 다음 index와, 그 다음 index 사이의 각도를 구하는 함수
     {
