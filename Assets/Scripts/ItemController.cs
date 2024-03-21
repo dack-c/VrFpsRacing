11c87885a97f9be2a27b76ca8cd57258ca3d33fc
BREG001 <ws4368@naver.com>
2024-03-21 13:19:28 +0900

    feat: ItemController 작업 4
    - 스크립트 내 설명 추가
    - 필요 함수 명시

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -10,24 +10,52 @@ public class ItemController : MonoBehaviour
 
     public int currentSlot = 0;
 
-    public void Init()
+    public void Start()
+    {
+        InitItemController();
+    }
+
+    public void InitItemController()
     {
         for (int i = 0; i < selectedItem.Length; i++)
             selectedItem[i] = noItem;
     }
 
+    /// <summary>
+    /// Call when using an item in the current slot.
+    /// </summary>
     public void UseItem()
     {
         if (selectedItem[currentSlot].Use())
-            ExpiredItem();
+            CleanCurrentSlot();
     }
 
-    public void ExpiredItem()
+    /// <summary>
+    /// Called when dropping an item in the currently selected item slot into the world space.
+    /// </summary>
+    public void DropItem()
+    {
+        // 이곳에 월드에 아이템을 스폰하는 함수 작성
+
+        CleanCurrentSlot();
+    }
+
+    /// <summary>
+    /// Call to remove an item from the currently selected item slot.
+    /// </summary>
+    public void CleanCurrentSlot()
     {
         selectedItem[currentSlot] = noItem;
         GameManager.I.Hud.ChangeSlotIcon(currentSlot, null);
     }
 
+    /// <summary>
+    /// Call when you get items that are dropped in the world space.
+    /// Returns the index number of the slot in which the item was obtained. 
+    /// And returns -1 if the item was not obtained because the inventory was full.
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
     public int GetDropedItem(ItemDefinition item)
     {
         for (int i = 0; i < selectedItem.Length; i++)
@@ -38,6 +66,9 @@ public class ItemController : MonoBehaviour
             {
                 selectedItem[i] = item;
                 GameManager.I.Hud.ChangeSlotIcon(i, item.itemIcon);
+
+                // 이곳에 플레이어에게 아이템을 장착시키는 함수 작성
+
                 return i;
             }
         }
@@ -45,13 +76,17 @@ public class ItemController : MonoBehaviour
         return -1;
     }
 
+    /// <summary>
+    /// Use to select the index-th item slot
+    /// </summary>
+    /// <param name="index"></param>
     public void SwitchItem(int index)
     {
         // return if there is no change in the existing slot
         if (currentSlot == index)
             return;
 
-        // 이곳에 플레이어가 장착한 아이템을 장착 해제하는 기능을 작성
+        // 이곳에 플레이어가 장착한 아이템을 장착 해제하는 함수 작성
 
         currentSlot = index;
 
@@ -64,7 +99,7 @@ public class ItemController : MonoBehaviour
 
         if (selectedItem[currentSlot].isEquipable)
         {
-            // 이곳에 플레이어에게 아이템을 장착시키는 기능을 작성
+            // 이곳에 플레이어에게 아이템을 장착시키는 함수 작성
         }
     }
 
@@ -72,13 +107,21 @@ public class ItemController : MonoBehaviour
     // for Item Slot Debug
     public void OnGUI()
     {
-        if (GUI.Button(new Rect(100, 150, 100, 30), "item_Left"))
+        if (GUI.Button(new Rect(100, 150, 100, 30), "Slop Left"))
         {
             SwitchItem(currentSlot - 1);
         }
-        if (GUI.Button(new Rect(200, 150, 100, 30), "item_Right"))
+        if (GUI.Button(new Rect(200, 150, 100, 30), "Slot Right"))
         {
             SwitchItem(currentSlot + 1);
+        }
+        if (GUI.Button(new Rect(300, 150, 100, 30), "Drop Item"))
+        {
+            DropItem();
+        }
+        if (GUI.Button(new Rect(400, 150, 100, 30), "Use Item"))
+        {
+            UseItem();
         }
     }
 #endif
