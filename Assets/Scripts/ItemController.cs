a10bbef0f38b7e69149ed7b8c8f8c3112f7ca05a
BREG001 <ws4368@naver.com>
2024-04-01 18:54:10 +0900

    feat: 아이템&아이템 컨트롤러 연동 작업1

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -34,16 +34,6 @@ public class ItemController : MonoBehaviour
     }
 
     /// <summary>
-    /// Called when dropping an item in the currently selected item slot into the world space.
-    /// </summary>
-    public void DropItem()
-    {
-        // 이곳에 월드에 아이템을 스폰하는 함수 작성
-
-        CleanCurrentSlot();
-    }
-
-    /// <summary>
     /// Call to remove an item from the currently selected item slot.
     /// </summary>
     public void CleanCurrentSlot()
@@ -51,31 +41,46 @@ public class ItemController : MonoBehaviour
         selectedItem[currentSlot] = noItem;
         GameManager.I.Hud.ChangeSlotIcon(currentSlot, null);
     }
+    
+    /// <summary>
+    /// Add the "item" in the "index"-th item slot when called.
+    /// </summary>
+    /// <param name="item"></param>
+    /// <param name="index"></param>
+    public void AddItemToSlot(ItemDefinition item, int index)
+    {
+        if (selectedItem[index].item == ItemDefinition.Item.None)
+        {
+            GameManager.I.Hud.ChangeSlotIcon(currentSlot, item.itemIcon);
+            selectedItem[currentSlot] = item;
+        }
+        else
+            Debug.Log($"There is already an item in the index-th item slot:{index}");
+    }
 
     /// <summary>
     /// Call when you get items that are dropped in the world space.
-    /// Returns the index number of the slot in which the item was obtained. 
-    /// And returns -1 if the item was not obtained because the inventory was full.
+    /// First check whether the current slot is empty, then check whether there are other empty slots in order from index 0.
+    /// If an empty slot is found, the index value of the corresponding slot is returned.
+    /// If there is no empty slot, return -1.
     /// </summary>
-    /// <param name="item"></param>
     /// <returns></returns>
-    public int GetDropedItem(ItemDefinition item)
+    public int CheckEmptyIndex()
     {
+        // check the current slot
+        if (selectedItem[currentSlot].item == ItemDefinition.Item.None)
+            return currentSlot;
+
+        // check the other empty slot
         for (int i = 0; i < selectedItem.Length; i++)
         {
-            if (selectedItem[i] == noItem)
+            if (selectedItem[i].item == ItemDefinition.Item.None)
                 continue;
             else
-            {
-                selectedItem[i] = item;
-                GameManager.I.Hud.ChangeSlotIcon(i, item.itemIcon);
-
-                // 이곳에 플레이어에게 아이템을 장착시키는 함수 작성
-
                 return i;
-            }
         }
-
+        
+        // no empty slot
         return -1;
     }
 
@@ -107,25 +112,14 @@ public class ItemController : MonoBehaviour
     }
 
 #if UNITY_EDITOR
-    // for Item Slot Debug
+    // for Item Slot Debug on Desktop
     public void OnGUI()
     {
-        if (GUI.Button(new Rect(100, 150, 100, 30), "Slop Left"))
-        {
+        if (GUI.Button(new Rect(100, 150, 100, 30), "Slot Left"))
             SwitchItem(currentSlot - 1);
-        }
+        
         if (GUI.Button(new Rect(200, 150, 100, 30), "Slot Right"))
-        {
             SwitchItem(currentSlot + 1);
-        }
-        if (GUI.Button(new Rect(300, 150, 100, 30), "Drop Item"))
-        {
-            DropItem();
-        }
-        if (GUI.Button(new Rect(400, 150, 100, 30), "Use Item"))
-        {
-            UseItem();
-        }
     }
 #endif
 }
